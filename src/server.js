879f7b00ca38951c64fb1c3beb6000c2dd3a25b3
Nirmal KC <thenirmalkc@gmail.com>
2021-11-27 22:09:42 +0545

    removed host env

--- src/server.js
+++ src/server.js
@@ -3,9 +3,8 @@
 require('dotenv').config();
 
 const app = require('./router');
-const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 
-app.listen(PORT, HOST, () => {
-  console.log('Listening on -> ' + HOST + ':' + PORT);
+app.listen(PORT, () => {
+  console.log('Listening on port ->' + PORT);
 });
